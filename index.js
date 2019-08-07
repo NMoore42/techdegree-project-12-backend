176682338d7b8be57ecb512c1e447d68f04e4487
NMoore42 <nicholas.moore5@icloud.com>
2019-08-07 16:46:46 -0500

    added mongoose connect code in index

--- index.js
+++ index.js
@@ -21,7 +21,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
 //Mongoose connection
-mongoose.connect("mongodb://localhost:27017/cmc-api");
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost:27017/cmc-api");
 const db = mongoose.connection;
 
 //Mongoose error
